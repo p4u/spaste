519200945307d0a6e818d9b3bc2bb4ec917c9471
p4u <p4u@dabax.net>
2019-01-28 11:09:16 +0100

    Change proto to HTTP 1.1

--- spaste.sh
+++ spaste.sh
@@ -9,7 +9,7 @@ URL="/add"
 echo $SERVER | egrep -q "[A-z]" && {
   SERVER=$(ping -c1 -w1 -I lo $SERVER 2>/dev/null  | grep ^PING | cut -d"(" -f2 | cut -d")" -f1)
 }
-echo -ne "POST $URL HTTP/1.0
+echo -ne "POST $URL HTTP/1.1
 Host: $HOST
 Content-Type:application/x-www-form-urlencoded
 Content-Length: ${BODY_LEN}\r\n
